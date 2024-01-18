53abca200faa9419a585306c256ddaafca15b2a7
yvonnkr <yvonnkr86@gmail.com>
2024-01-18 16:54:14 +0000

    init commit

---
+++ src/test/java/com/yvolabs/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
