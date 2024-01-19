a00747495b650f708a93a0d7e1137d5004580d10
yvonnkr <yvonnkr86@gmail.com>
2024-01-19 14:23:34 +0000

    setup structure and add models

---
+++ src/main/java/com/yvolabs/movies/model/Review.java
@@ -0,0 +1,20 @@
+package com.yvolabs.movies.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
